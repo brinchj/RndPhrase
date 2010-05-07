c48718cbe2414d4e06359e9a11271a50ae65d5e7
Johan Brinch <brinchj@gmail.com>
2010-05-07 12:16:44 +0100

    Hash seed before storing.

--- chrome/overlay.js
+++ chrome/overlay.js
@@ -3,7 +3,10 @@
 chrome.extension.sendRequest({name: "getPreferences"},
     function(response)
     {
-        rndphrase.RndPhrase.seed = response.prefSeed;
+        var seed = response.prefSeed;
+        if(seed != null && seed.length > 0) {
+            rndphrase.RndPhrase.seed_hash = seed;
+        }
         rndphrase.RndPhrase.patch_document(document.location.host, document);
     });
 
