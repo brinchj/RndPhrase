040e9c3ec7580b1134a5cb1459dcfa250b64d5a3
Johan Brinch <brinchj@gmail.com>
2010-05-07 12:17:01 +0100

    Default seed is empty.

--- firefox/defaults/preferences/rndphrase.js
+++ firefox/defaults/preferences/rndphrase.js
@@ -1,3 +1,3 @@
-pref("extensions.rndphrase.seedpref", "1234");
 // See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.rndphrase.seedpref", "");
 pref("extensions.rndphrase@firefox.description", "chrome://rndphrase/locale/rndphrase.properties");
