cf7cfa04caecf71218695b4f5f285d7d92aef9cd
Johan Brinch <brinchj@gmail.com>
2010-06-09 13:39:55 +0100

    Added build of ZIP file for Google submission

--- chrome/build.sh
+++ chrome/build.sh
@@ -5,7 +5,13 @@ if (pgrep chrome); then
     exit 1;
 fi
 
+# CRX addon
 mkdir -p src build
 cp *.html *.js *.json src
 ${CHROME_BIN} --pack-extension=src --pack-extension-key=unofficial_key.pem
 cp src.crx build/rndphrase.crx
+
+# ZIP file
+cd src
+zip ../build/rndphrase.zip *
+cd ..
