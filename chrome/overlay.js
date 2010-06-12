3c577d507d2884f8fd38b2e118dfcdb0edca403f
Johan Brinch <brinchj@gmail.com>
2010-06-12 12:26:51 +0100

    Check before using components

--- chrome/overlay.js
+++ chrome/overlay.js
@@ -3,6 +3,9 @@
 chrome.extension.sendRequest({name: "getPreferences"},
     function(response)
     {
+        if(!rndphrase.RndPhrase.self_test()) {
+            throw "Self test failed!";
+        }
         var seed = response.prefSeed;
         if(seed != null && seed.length > 0) {
             rndphrase.RndPhrase.seed_hash = seed;
