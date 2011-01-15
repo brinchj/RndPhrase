960105e28ba7e33de29cc23df9377abce357ac79
Johan Brinch <brinchj@gmail.com>
2011-01-15 21:12:18 +0100

    Don't wait for page to load

--- js/js/rndphrase.js
+++ js/js/rndphrase.js
@@ -1,12 +1,12 @@
 #include "lib/rndphrase.js"
 
-window.addEventListener('load', function() {
-    if(!rndphrase.RndPhrase.self_test()) {
-        throw "Self test failed!";
-    } else {
-        rndphrase.RndPhrase.patch_document(document.location.host, document);
-    }
-}, false);
-
 // configure the seed here or by sometime later depending on platform
 rndphrase.RndPhrase.seed = "";
+
+if(!rndphrase.RndPhrase.self_test()) {
+    throw "Self test failed!";
+} else {
+    rndphrase.RndPhrase.patch_document(document.location.host, document);
+}
+
+
