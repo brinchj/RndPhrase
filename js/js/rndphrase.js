a105a7ffa0bbc0ea159b33d8efa80087e0288f63
Johan Brinch <brinchj@gmail.com>
2011-01-17 14:01:41 +0100

    Add space after "if"

--- js/js/rndphrase.js
+++ js/js/rndphrase.js
@@ -3,7 +3,7 @@
 // configure the seed here or by sometime later depending on platform
 rndphrase.RndPhrase.seed = "";
 
-if(!rndphrase.RndPhrase.self_test()) {
+if (!rndphrase.RndPhrase.self_test()) {
     throw "Self test failed!";
 } else {
     rndphrase.RndPhrase.patch_document(document.location.host, document);
