61f25516cbf3ce6161bed44c83fa1b85143c9ec4
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-07-21 17:28:27 +0200

    Made the module more compatible withstuff

--- js/rndphrase.js
+++ js/rndphrase.js
@@ -1,3 +1,18 @@
-#include "lib/rndphrase.js"
-
-exports.rndphrase = rndphrase;
+/*jslint bitwise: true*/
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+    }
+}(this, function () {
+	#include "lib/rndphrase.js"
+	return rndphrase;
+}));
